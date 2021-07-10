d52239a87cc9ec986563a8d80e205375ecfa4919
Budi Darmawan <budi20.darmawan20@gmail.com>
2021-07-10 17:38:58 +0700

    Initial Commit

---
+++ Game Catalogue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game Catalogue
+//
+//  Created by Budi Darmawan on 10/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
